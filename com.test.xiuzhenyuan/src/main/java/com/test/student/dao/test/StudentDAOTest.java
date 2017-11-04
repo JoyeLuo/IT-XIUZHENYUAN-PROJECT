356d395391036ba4ddbb02f9abdda685ef62e020
Joye Luo <joye.luo@liferay.com>
2017-11-04 23:08:07 +0800

    add C3P0 database connection pool for project

--- com.test.xiuzhenyuan/src/main/java/com/test/student/dao/test/StudentDAOTest.java
+++ com.test.xiuzhenyuan/src/main/java/com/test/student/dao/test/StudentDAOTest.java
@@ -4,6 +4,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.test.student.dao.IStudentDAO;
+import com.test.student.model.Student;
 
 public class StudentDAOTest {
 
@@ -11,6 +12,22 @@ public class StudentDAOTest {
 		ApplicationContext applicationContext=new ClassPathXmlApplicationContext("applicationContext.xml");
 		IStudentDAO dao=(IStudentDAO) applicationContext.getBean("studentDao");
 
+		/*Student student = new Student();
+		student.setId(6);
+		student.setName("小红");
+		dao.addStudent(student);
+
+		Student student1 = new Student();
+		student1.setId(7);
+		student1.setName("小明");
+		dao.addStudent(student1);
+
+		dao.deleteStudentById(6);
+
+		System.out.println(dao.seachStudentNameById(7));*/
+		for (int i = 0; i <= 1000; i++) {
+			System.out.println(dao.seachStudentNameById(1));
+		}
 	}
 
 }
